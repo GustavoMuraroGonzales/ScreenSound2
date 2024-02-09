e9dc5272a574bf41fb833c6c996ad0ef8b2ac1ee
GustavoGonzales <gustavo.gonzales.tec@gmail.com>
2024-02-09 16:45:44 -0300

    Criado album e ligação das musicas com o album e seus respectivos tempos

--- ScreenSound2/Program.cs
+++ ScreenSound2/Program.cs
@@ -1,19 +1,15 @@
-﻿Musica musica1 = new Musica();
-musica1.Nome = "Roxane";
-musica1.Artista = "The Police";
-musica1.Duracao = 273;
-musica1.Disponivel = true;
-Console.WriteLine(musica1.Disponivel);
+﻿Album albumDoQueen = new Album();
+albumDoQueen.Nome = "A night of the opera";
 
+Musica musica1 = new Musica();
+musica1.Nome = "Love of my life";
+musica1.Duracao = 213;
 
 Musica musica2 = new Musica();
-musica2.Nome = "Vertigo";
-musica2.Artista = "U2";
-musica2.Duracao = 367;
-musica2.Disponivel = false;
-Console.WriteLine(musica2.Disponivel);
+musica2.Nome = "Bohemian Rhapsody";
+musica2.Duracao = 354;
 
+albumDoQueen.AdicionarMusica(musica1);
+albumDoQueen.AdicionarMusica(musica2);
 
-//musica1.ExibirFichaTecnica();
-//musica2.ExibirFichaTecnica();
-
+albumDoQueen.ExibirMusicasDoAlbum();
