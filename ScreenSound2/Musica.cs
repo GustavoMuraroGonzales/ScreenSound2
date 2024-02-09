4f25fe8531f794e8b7cb5c642ac023b5a30e743e
GustavoGonzales <gustavo.gonzales.tec@gmail.com>
2024-02-09 14:57:05 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound2/Musica.cs
@@ -0,0 +1,28 @@
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel == true)
+        {
+            Console.WriteLine($"A música {nome} está disponivel em seu plano!");
+        }
+        else
+        {
+            Console.WriteLine($"A música {nome} não está disponivel em seu plano!");
+        }
+    }
+
+    public void ExibirNomeArtista()
+    {
+        Console.WriteLine($"Nome/Artista: {nome} - {artista}");
+    }
+
+}
---
+++ ScreenSound2/Program.cs
@@ -0,0 +1,18 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+musica1.duracao = 273;
+musica1.disponivel = false;
+
+
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+musica2.duracao = 367;
+musica2.disponivel = true;
+
+//musica1.ExibirFichaTecnica();
+//musica2.ExibirFichaTecnica();
+
+musica2.ExibirNomeArtista();
