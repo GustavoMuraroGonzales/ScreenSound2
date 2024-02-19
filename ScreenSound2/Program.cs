13db55a77ed72dacbe6a49558e2c2b4c144d16d8
GustavoGonzales <gustavo.gonzales.tec@gmail.com>
2024-02-19 18:15:52 -0300

    Concluido o Curso, com o desafio final

--- ScreenSound2/Program.cs
+++ ScreenSound2/Program.cs
@@ -1,24 +1,23 @@
-﻿Banda queen = new Banda("Queen");
+﻿Episodio ep1 = new(4, "Técnicas de Facilitação", 45);
+ep1.AdicionarConvidados("Ana Pereira");
+ep1.AdicionarConvidados("Mário Francis");
 
-Album albumDoQueen = new Album("A night at the opera");
+Episodio ep2 = new(2, "Aprendendo a aprender", 78);
+ep2.AdicionarConvidados("Marcos Felício");
 
-Musica musica1 = new Musica(queen, "Love of my life")
-{
-    Duracao = 213,
-    Disponivel = true,
-};
+Episodio ep3 = new(3, "Consciênciologia", 87);
+ep3.AdicionarConvidados("Flavio Almeida");
+ep3.AdicionarConvidados("Gui Lima");
+ep3.AdicionarConvidados("Fernanda Fernandes");
 
-Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
-{
-    Duracao = 354,
-    Disponivel = false,
-};
+Episodio ep0 = new(1, "Filosofia de software", 93);
+ep0.AdicionarConvidados("Fernando Roberto");
+ep0.AdicionarConvidados("Gabriel Barbosa");
 
-albumDoQueen.AdicionarMusica(musica1);
-albumDoQueen.AdicionarMusica(musica2);
-queen.AdicionarAlbum(albumDoQueen);
+Podcast podcast = new("TI para Poucos", "Daniel Portugal");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.AdicionarEpisodio(ep3);
+podcast.AdicionarEpisodio(ep0);
 
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
-albumDoQueen.ExibirMusicasDoAlbum();
-queen.ExibirDiscografia();
+podcast.ExibirDetalhes();
